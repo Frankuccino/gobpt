b5b1c79f5e7faed95473eb53c9cd80d24b723097
Frank Angelo Malubag <frankabad189@gmail.com>
2026-07-06 14:19:25 +0800

    feat(config): add configuration management for observability and embed in main Config
    This includes adding packages: validator, koanf env, koanf, zerolog &
    godotenv.
    - embed Observability struct in Config as pointer for easier nil check
    on uninitialized state.
    
    Changes added to LoadConfig:
    - initialize default observability config
    - set observability config ServiceName and Environment
    - validate observability config by invoking Validate of Observalibity.

--- backend/internal/config/config.go
+++ backend/internal/config/config.go
@@ -11,11 +11,12 @@ import (
 )
 
 type Config struct {
-	Primary  Primary        `koanf:"primary" validate:"required"`
-	Server   ServerConfig   `koanf:"server" validate:"required"`
-	Database DatabaseConfig `koanf:"database" validate:"required"`
-	Auth     AuthConfig     `koanf:"auth" validate:"required"`
-	Redis    RedisConfig    `koanf:"redis" validate:"requires"`
+	Primary       Primary              `koanf:"primary" validate:"required"`
+	Server        ServerConfig         `koanf:"server" validate:"required"`
+	Database      DatabaseConfig       `koanf:"database" validate:"required"`
+	Auth          AuthConfig           `koanf:"auth" validate:"required"`
+	Redis         RedisConfig          `koanf:"redis" validate:"requires"`
+	Observability *ObservabilityConfig `koanf:"observability"`
 }
 
 type Primary struct {
@@ -77,6 +78,20 @@ func LoadConfig() (*Config, error) {
 	err = validate.Struct(mainConfig)
 	if err != nil {
 		logger.Fatal().Err(err).Msg("config validation failed")
+	}
+
+	// set default observability config if not provided
+	if mainConfig.Observability == nil {
+		mainConfig.Observability = DefaultObservabilityConfig()
+	}
+
+	// Overried service name and environment from primary config
+	mainConfig.Observability.ServiceName = "boilerplate"
+	mainConfig.Observability.Environment = mainConfig.Primary.Env
+
+	// Validate observability config
+	if err := mainConfig.Observability.Validate(); err != nil {
+		logger.Fatal().Err(err).Msg("invalid observability config")
 	}
 
 	return mainConfig, nil
